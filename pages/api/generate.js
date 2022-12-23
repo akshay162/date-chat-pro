dd51b6b5e61dbe8b6cfc9b57246eb787c8a91a81
akshay162 <akshay.gpt1602@gmail.com>
2022-12-24 00:14:11 +0530

    changes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,10 +10,10 @@ const openai = new OpenAIApi(configuration);
 // Take the conversation forward in a funnny and witty way imagining the conversation is for a date.
 
 let prompts = [
-  "Take the conversation forward in a funnny and witty way imagining the conversation is for a date. Reply for he only.\n",
+  "Take the conversation forward in a funnny and witty way imagining the conversation is for a date. They got matched on a dating platform for the first time and this is their first conversation. Reply for he only.\n",
   "Take the conversation forward in a funny and witty way. They are looking to get to know each other better so they can fall in love if interests align. Imagine they are talking the first time and that too in different cities. Reply for he only.\n"
 ]
-const basePromptPrefix = prompts[1];
+const basePromptPrefix = prompts[0];
 
 const generateAction = async (req, res) => {
 
